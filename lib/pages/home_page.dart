4bd251230ba2c14dcfbf42add5d30f3ea7687429
Naufal Athallah Iwel <naufal@klinikpintar.id>
2023-06-28 17:35:53 +0700

    feat: feed & profile page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:finstagram/pages/feed_page.dart';
+import 'package:finstagram/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,33 +11,36 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentPage = 0;
-
+  final List<Widget> _pages = [
+    FeedPage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Finstagram",
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: const Icon(Icons.add_a_photo),
+        appBar: AppBar(
+          title: const Text(
+            "Finstagram",
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 8.0,
-              right: 8.0,
-            ),
-            child: GestureDetector(
+          actions: [
+            GestureDetector(
               onTap: () {},
-              child: const Icon(Icons.logout),
+              child: const Icon(Icons.add_a_photo),
             ),
-          )
-        ],
-      ),
-      bottomNavigationBar: _bottomNavigationBar(),
-    );
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 8.0,
+                right: 8.0,
+              ),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Icon(Icons.logout),
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: _bottomNavigationBar(),
+        body: _pages[_currentPage]);
   }
 
   Widget _bottomNavigationBar() {
