8577da7af94ed367528e88e24ce0ffe16d7ff9f8
Naufal Athallah Iwel <naufal@klinikpintar.id>
2023-06-28 16:27:31 +0700

    feat: home icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:finstagram/pages/home_page.dart';
 import 'package:finstagram/pages/login_page.dart';
 import 'package:finstagram/pages/register_page.dart';
 import 'package:flutter/material.dart';
@@ -16,10 +17,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      initialRoute: 'login',
+      initialRoute: 'home',
       routes: {
         'register': (context) => RegisterPage(),
-        'login': (context) => LoginPage()
+        'login': (context) => LoginPage(),
+        'home': (context) => HomePage(),
       },
     );
   }
